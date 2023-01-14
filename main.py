f0547a1c09a2a980f03e60c8d8632cd9c54b7062
tallurirohan <113008429+tallurirohan@users.noreply.github.com>
2023-01-14 12:29:04 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+from tkinter import *
+from tkinter.filedialog import *
+import tkinter as tk
+
+def saveFile():
+    new_file = asksaveasfile(mode='w',filetypes=[('text files','.txt')])
+    if new_file is None:
+        return
+    text = str(entry.get(1.0,END))
+    new_file.write(text)
+    new_file.close()
+
+def openFile():
+    file = askopenfile(mode='r',filetypes=[('text files','.txt')])
+    if file is not None:
+        content = file.read()
+    entry.insert(INSERT,content)
+
+def clearFile():
+    entry.delete(1.0,END)
+canvas = tk.Tk()
+canvas.geometry("400x600")
+canvas.title("Notepad")
+canvas.config(bg="#f4dc72")
+top = Frame(canvas)
+top.pack(padx=10, pady=5, anchor='nw')
+
+b1 = tk.Button(canvas, text="open", bg="white",command=openFile)
+b1.pack(in_=top, side=LEFT)
+
+b2 = tk.Button(canvas, text="save", bg="white",command = saveFile)
+b2.pack(in_=top, side=LEFT)
+
+b3 = tk.Button(canvas, text="clear", bg="white",command = clearFile)
+b3.pack(in_=top, side=LEFT)
+
+b4 = tk.Button(canvas, text="exit", bg="white",command = exit)
+b4.pack(in_=top, side=LEFT)
+
+entry = Text(canvas, wrap=WORD, bg="#ecba13", font=("poppins", 15))
+entry.pack(padx=10, pady=5,expand =TRUE,fill=BOTH)
+canvas.mainloop()
